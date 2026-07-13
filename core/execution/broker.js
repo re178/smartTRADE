58b38ec3928e8997488397e2dc82377cc3a77cb5
re178 <esilesirayland@gmail.com>
2026-07-13 18:03:43 +0300

    Update broker.js

--- core/execution/broker.js
+++ core/execution/broker.js
@@ -4,7 +4,7 @@ const WebSocket = require('ws');
 const { EventEmitter } = require('events');
 const { sleep } = require('../../shared/helpers');
 const logger = require('../../infrastructure/logger') || console;
-const Order = require('../../../models/Order');
+const Order = require('../../models/Order');
 
 // Set log level to debug temporarily (or set LOG_LEVEL=debug in env)
 // If your logger supports levels, you can enable debug output.
