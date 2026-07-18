4032a5aaaf824b22d991758ceac93a36eba79780
re178 <esilesirayland@gmail.com>
2026-07-18 03:22:38 +0300

    Add Mt5CommandResult model schema

---
+++ models/Mt5CommandResult.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Mt5CommandResultSchema = new mongoose.Schema({
+  commandId: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+  },
+  success: Boolean,
+  ticket: Number,
+  deal: Number,
+  price: Number,
+  volume: Number,
+  symbol: String,
+  side: String,
+  retcode: Number,
+  retcodeDescription: String,
+  error: String,
+  time: Number,            // timestamp from EA
+  receivedAt: {
+    type: Date,
+    default: Date.now,
+    expires: 3600,         // auto-delete after 1 hour
+  },
+});
+
+module.exports = mongoose.model('Mt5CommandResult', Mt5CommandResultSchema);
